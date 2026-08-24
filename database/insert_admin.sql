febb496eeaa9d43a7c60794dc9bf46e2c238533f
Laurena Rakotomalala <hei.laurena@gmail.com>
2026-08-24 18:39:53 +0300

    Update default admin credentials

--- database/insert_admin.sql
+++ database/insert_admin.sql
@@ -1,9 +1,3 @@
 INSERT INTO users (name, email, password_hash, role, is_active)
-VALUES (
-  'Super Admin',
-  'admin@examhub.local',
-  '$2b$10$BdO1MRy.hx91lFRsluglM.AGMm0hcRHcQ4qNsnBEPPGjadDn3vB0e',
-  'ADMIN',
-  true
-)
+VALUES ('Super Admin', 'admin@gmail.com', '$2b$10$sq6MHxTOwKxuqZZgCvINPuFcw0fp7rI0i8tgvmYa71TpbdZ03C5.S', 'ADMIN', true)
 ON CONFLICT (email) DO NOTHING;
